8df948fc6e2eaef4f11bc3738733f9a2f8c55b6c
Adilson <adilsonpsaquino@gmail.com>
2024-12-07 09:09:28 -0300

    Container com data e hora

--- js/atualizar_Valores.js
+++ js/atualizar_Valores.js
@@ -23,3 +23,29 @@ function atualizarLED(valor) {
     const LEDElemento = document.getElementById("led");
     LEDElemento.textContent = `Estado do LED: ${valor}`; // usar valor entre aspas tipo atualizarLED("ligado")
 }
+
+function updateTimeAndDate() {
+    const timeElement = document.getElementById("time");
+    const dateElement = document.getElementById("date");
+
+    const now = new Date();
+
+    // Hora atual formatada (HH:MM:SS)
+    const timeString = now.toLocaleTimeString();
+
+    // Data atual formatada (Ex: Segunda-feira, 7 de Dezembro de 2024)
+    const dateString = now.toLocaleDateString("pt-BR", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
+    timeElement.textContent = timeString;
+    dateElement.textContent = dateString;
+}
+
+// Atualizar a hora e a data a cada segundo
+setInterval(updateTimeAndDate, 1000);
+updateTimeAndDate();
+
