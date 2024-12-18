fd874c3d560b7e41c0cc77c3ba38bb1eca9c21bc
e-rick23 <erickmarques63@gmail.com>
2024-12-18 09:32:45 -0300

    Adicionados comentários

--- js/configuracao.js
+++ js/configuracao.js
@@ -1,13 +1,15 @@
+//Script do menu de configurações.
+
 document.addEventListener("DOMContentLoaded", () => {
     const settingsMenu = document.getElementById("settings-menu");
     const openSettingsButton = document.getElementById("open-settings");
     const closeSettingsButton = document.getElementById("close-settings");
 
     openSettingsButton.addEventListener("click", () => {
-        settingsMenu.classList.add("open");
+        settingsMenu.classList.add("open"); //Ao clicar abre as configurações
     });
 
     closeSettingsButton.addEventListener("click", () => {
-        settingsMenu.classList.remove("open");
+        settingsMenu.classList.remove("open"); //Ao clicar no botão de fechar, fecha o menu.
     });
 });
